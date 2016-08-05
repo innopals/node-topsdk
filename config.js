e58d3217222417aa5589a3bae526f201c7e9910d
Cao Jiannan <frogcjn@163.com>
2016-08-05 10:33:51 +0800

    add endpoint_https

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   endpoint: process.env.TEST_TOP_ENDPOINT || 'http://gw.api.tbsandbox.com/router/rest',
+  endpoint_https: process.env.TEST_TOP_ENDPOINT || 'https://gw.api.tbsandbox.com/router/rest',
   app_key: process.env.TEST_TOP_APP_KEY || '1023307190',
   app_secret: process.env.TEST_TOP_APP_SECRET || 'sandbox01eff1e5ac77cf3d078982e6e',
   session: process.env.TOPSDK_TEST_SESSION || '61019148c6ec24ead112b7109e0345ab97b3067583533ba2074082786'
